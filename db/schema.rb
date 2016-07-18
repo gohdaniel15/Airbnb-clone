395fc5579ec96892109dbccb1154bfc445aa9095
gohdaniel15 <gohdaniel15@gmail.com>
2016-07-18 13:26:27 +0800

    Added listings and root page

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160718035000) do
+ActiveRecord::Schema.define(version: 20160718043006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "listings", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "address"
+    t.string   "city"
+    t.string   "country"
+    t.string   "property_type"
+    t.integer  "no_of_beds"
+    t.integer  "no_of_bathrooms"
+    t.text     "description"
+    t.text     "price"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                     null: false
